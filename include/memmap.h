d1b198d9236dfa51d37e441228ed1ba93a97999b
tantruong215 <idyetan@gmail.com>
2025-06-08 20:43:06 -0400

    Update memmap.h

---
+++ include/memmap.h
@@ -0,0 +1,11 @@
+// include/memmap.h
+#ifndef MEMMAP_H
+#define MEMMAP_H
+
+#define UART0_BASE   0x40001000U
+#define SPI0_BASE    0x40002000U
+#define GPIO_BASE    0x40003000U
+#define RAM_START    0x20000000U
+#define RAM_SIZE     0x00001000U  // 4 KB
+
+#endif // MEMMAP_H
