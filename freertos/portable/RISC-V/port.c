3794dea2fcf55e1cc46e5a22987dc3d50da27f75
Tan Truong <tantruong215@gmail.com>
2025-06-09 23:23:13 -0400

    Add FreeRTOS RISC-V port files (portmacro.h, port.c, portasm.S)

---
+++ freertos/portable/RISC-V/port.c
@@ -0,0 +1,53 @@
+﻿#include "FreeRTOS.h"
+#include "task.h"
+#include "portmacro.h"
+
+#define CLINT_BASE       0x02000000UL
+#define MTIMECMP_OFFSET  0x4000
+#define MTIME_OFFSET     0xBFF8UL
+
+volatile uint64_t * const mtime    = (uint64_t*)(CLINT_BASE + MTIME_OFFSET);
+volatile uint64_t * const mtimecmp = (uint64_t*)(CLINT_BASE + MTIMECMP_OFFSET);
+
+void FreeRTOS_Tick_Handler( void );
+static void prvSetupTimerInterrupt( void );
+
+BaseType_t xPortStartScheduler( void )
+{
+    prvSetupTimerInterrupt();
+    vPortStartFirstTask();
+    return 0;
+}
+
+static void prvSetupTimerInterrupt( void )
+{
+    uint64_t ticks = configCPU_CLOCK_HZ / configTICK_RATE_HZ;
+    *mtimecmp = *mtime + ticks;
+    __asm volatile( "csrsi mie, 0x80" );
+    __asm volatile( "csrsi mstatus, 0x8" );
+}
+
+void FreeRTOS_Tick_Handler( void )
+{
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+    uint64_t ticks = configCPU_CLOCK_HZ / configTICK_RATE_HZ;
+    *mtimecmp += ticks;
+    if( xTaskIncrementTick() != pdFALSE )
+    {
+        xHigherPriorityTaskWoken = pdTRUE;
+    }
+    if( xHigherPriorityTaskWoken != pdFALSE )
+    {
+        portYIELD();
+    }
+}
+
+void vPortYield( void )
+{
+    __asm volatile( "ecall" );
+}
+
+void vPortEndScheduler( void )
+{
+    for( ;; );
+}
---
+++ freertos/portable/RISC-V/portmacro.h
@@ -0,0 +1,40 @@
+﻿#ifndef PORTMACRO_H
+#define PORTMACRO_H
+
+#include <stdint.h>
+
+#define portCHAR        char
+#define portFLOAT       float
+#define portDOUBLE      double
+#define portLONG        long
+#define portSHORT       short
+#define portSTACK_TYPE  uint32_t
+#define portBASE_TYPE   long
+
+typedef portSTACK_TYPE StackType_t;
+typedef long BaseType_t;
+typedef unsigned long UBaseType_t;
+
+#define portSTACK_GROWTH            ( -1 )
+#define portTICK_PERIOD_MS          ( ( TickType_t ) 1000 / configTICK_RATE_HZ )
+#define portBYTE_ALIGNMENT          8
+
+#define portENTER_CRITICAL()     __asm volatile( "csrci mstatus, 8" ::: "memory" )
+#define portEXIT_CRITICAL()      __asm volatile( "csrsi mstatus, 8" ::: "memory" )
+#define portYIELD()              __asm volatile( "ecall" ::: "memory" )
+#define portDISABLE_INTERRUPTS() __asm volatile( "csrci mstatus, 8" ::: "memory" )
+#define portENABLE_INTERRUPTS()  __asm volatile( "csrsi mstatus, 8" ::: "memory" )
+
+#if ( configUSE_16_BIT_TICKS == 1 )
+    typedef uint16_t TickType_t;
+    #define portMAX_DELAY ( TickType_t ) 0xffff
+#else
+    typedef uint32_t TickType_t;
+    #define portMAX_DELAY ( TickType_t ) 0xffffffffUL
+#endif
+
+extern void vPortStartFirstTask( void );
+extern void vPortYield( void );
+#define FreeRTOS_Tick_Handler MachineTimer_IRQHandler
+
+#endif /* PORTMACRO_H */
